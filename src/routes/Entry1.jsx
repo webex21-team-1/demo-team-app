f472350a993ce78ac0b2084899fb9c94f3f0e9b8
yamamoto <k221754@kansai-u.ac.jp>
2024-02-21 18:45:29 +0900

    Entry1にボタンを追加しました

--- src/routes/Entry1.jsx
+++ src/routes/Entry1.jsx
@@ -1,8 +1,27 @@
+function Square() {
+  return <button className="square">　</button>;
+}
+
 export const Entry1 = () => {
   return (
     <>
       <h1>やまっちの作業</h1>
       <div>エントリー1の内容</div>
+      <div className="board-row">
+        <Square></Square>
+        <Square></Square>
+        <Square></Square>
+      </div>
+      <div className="board-row">
+        <Square></Square>
+        <Square></Square>
+        <Square></Square>
+      </div>
+      <div className="board-row">
+        <Square></Square>
+        <Square></Square>
+        <Square></Square>
+      </div>
     </>
   );
 };
