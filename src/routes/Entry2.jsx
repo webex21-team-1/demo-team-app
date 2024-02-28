1cdba6e4ac8f38dd24a1a7f5b35fd446b5b67ab3
Yuji <yuji08752@gmail.com>
2024-02-28 23:43:20 +0900

    Revert "カッコのエラーが分かりまんでしたがとりあえずセーブします"
    This reverts commit 200726ff9fa1dece5f9db3c2c9ebdeb32dfdb452.

--- src/routes/Entry2.jsx
+++ src/routes/Entry2.jsx
@@ -1,78 +1,8 @@
-function attendance() {
-  return <button className="attendance"></button>;
-}
-import { useState } from "react";
-
 export const Entry2 = () => {
-  function App(){
-    const[cards, setCards]=useState([
-      {
-        id: crypto.randomUUID(),
-        text:"久山陽平"
-      },
-    ]);
-    const [inputValue,setInputValue]=useState("")
-  
-  
-    return (
-     <>
+  return (
+    <>
       <h1>陽平の作業</h1>
       <div>エントリー2の内容</div>
-      <h1>出席管理</h1>
-      <div className="add">
-        <div className="main-container">
-          <div className="list-container">
-            <div className="list-header">出席リスト</div>
-            <div className="card-container">
-            {cards.map((card) =>(
-              <div className="card" key={card.id}>
-              <div className="atPeople">{card.text}</div>
-              <div className="delete"
-              onClick={() => {
-                const updatedCards = cards.filter((tempCard) => {
-                  return tempCard.id !== card.id;
-                });
-                setCards(updatedCards);
-              }}
-            />
-            
-              </div>
-              ))
-            }
-    </div>
-    <div className="list-footer">
-      <div className="list-container">
-        <input
-        type="text"
-        className="input-atPeople"
-        value={input-value}
-        onInput={(e) => {
-        setInputValue(e.target.value);
-        
-        }
-      }
-      />
-      <div
-      className="input-button"
-      onClick={()=>{
-        if(inputValue==="")return;
-        setCards([
-          ...cards,
-          {id:crypto.randomUUID(), text:inputValue},
-        ]);
-        setInputValue("");
-      }
-      }
-      >
-        出席
-      </div>
-      </div>
-      </div>
-        </div>
-        <attendance>出席</attendance>
-      </div>
-      </>
-    
-    );
-  }
-}
+    </>
+  );
+};
